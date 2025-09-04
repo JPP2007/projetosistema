dbb92df68f5dd8b3acd0bbf8711ad8dcb35feecb
João <joao34palmeira.28@gmail.com>
2025-09-04 09:46:14 -0300

    Adicionando api cep  na página cliente

--- src/routes/Cliente.jsx
+++ src/routes/Cliente.jsx
@@ -1,9 +1,62 @@
-import 
+import { useForm } from "react-hook-form";
+import { FcRegisteredTrademark } from "react-icons/fc";
 
 const Cliente = () => {
-  return (
-    <div>Cliente</div>
-  )
-}
 
-export default Cliente
+  // desestruturando o objeto
+  const {register,setValue,setFocus} = useForm();
+
+  // CRIANDO A FUNÇÃO DA API QUE VAI CONSUMIR OS DADOS
+
+  function buscarCep(e){
+    const cep= e.target.value;
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then((res)=>res.json())
+    .then((dados)=>{
+      setValue("rua",dados.logradouro);
+      setValue("bairro",dados.bairro);
+      setFocus("numero");
+    })
+  }
+
+
+  return (
+    <div>
+      <form>
+        <fieldset>
+          <legend>Dados Cliente</legend>
+          <div>
+            <label>Cep:</label>
+            <input type="text"
+            {...register("cep")}
+            onBlur={buscarCep}
+            />
+          </div>
+
+          <div>
+            <label>Rua:</label>
+            <input type="text"
+            {...register("rua")}
+            />
+          </div>
+
+          <div>
+            <label>Bairro:</label>
+            <input type="text"
+            {...register("bairro")}
+            />
+          </div>
+
+            <div>
+            <label>Número:</label>
+            <input type="text"
+              {...register("numero")}
+            />
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  );
+};
+
+export default Cliente;
